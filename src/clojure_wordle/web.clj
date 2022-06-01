eaf33c4ec9e3c83bd84ef8578c674aa7e6da3886
fj <fj@Viajero>
2022-06-02 00:08:44 +0900

    Fix web function. - game win judgement - game lose judgement

--- src/clojure_wordle/web.clj
+++ src/clojure_wordle/web.clj
@@ -15,7 +15,13 @@
             [clojure-wordle.wordle :as wordle]))
 
 (defonce history (atom '[]))
+(defonce valid-keys (atom '[]))
 (defonce secret (atom nil))
+(defonce win (atom false))
+
+(defn game-continue?
+  []
+  (and (< (count @history) 6) (not @win)))
 
 ;; root web page.
 (defn root-view [req]
@@ -28,9 +34,13 @@
     (hf/form-to
      [:post "/"]
      [:input {:name :guess :maxlength 5 :minlength 5 :required true :placeholder "input 5 charactor"}]
-     [:button "send"])
+     [:button (if (not (game-continue?) ) '{:disabled true}) "send"])
     (when-let [{:keys [msg]} (:flash req)] ;; リクエストマップに :flash があればそれをアラートとして表示される
       [:div.alart [:strong msg]])
+    (when @win
+      [:div [:strong "You Win !!"]])
+    (when (and (not @win) (<= 6 (count @history)))
+      [:div [:strong "You Lose !!"]])
     [:div
      (for [h @history]
        [:div 
@@ -50,16 +60,22 @@
 
 ;; judge web page.
 (defn judge-method [res req]
-  (if (wordle/word-valid? (:guess (:params req)) w/words)
+  (if (not (wordle/word-valid? (:guess (:params req)) w/words))
+    (assoc res :flash {:msg "Invalid word"})
     (do
       (swap! history conj (wordle/score (:guess (:params req)) @secret))
-      res)
-    (assoc res :flash {:msg "Invalid word"})))
+      (reset! valid-keys (wordle/available-keys @history))
+      (if (= (:guess (:params req)) @secret)
+        (reset! win true))
+      res)))
 
 (defn judge [req]
+  (if (game-continue?)
     (-> (res/redirect "/")
-      (judge-method req)
-      (res/content-type "text/html; charset=utf-8")))
+        (judge-method req)
+        (res/content-type "text/html; charset=utf-8"))
+    (-> (res/redirect "/")
+        (res/content-type "text/html; charset=utf-8"))))
 ;; judge web page end.
 
 ;; router & middleware
@@ -86,6 +102,8 @@
   (when-not @server
     (reset! server (server/run-jetty #'app {:port 3000 :join? false})))
   (reset! history '[])
+  (reset! valid-keys '[])
+  (reset! win false)
   (reset! secret (rand-nth w/words)))
 
 (defn stop-server []
